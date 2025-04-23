998876d27690209ff528bd09abfcd2367e7c8c54
Pabl1k <poiumazaya@gmail.com>
2025-04-23 10:36:27 +0300

    image optimization plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/user-card',
-  plugins: [react()]
+  plugins: [
+    react(),
+
+    ViteImageOptimizer({
+      test: /\.(jpeg|svg)$/i,
+      includePublic: true,
+      jpeg: {
+        quality: 70,
+        chromaSubsampling: '4:4:4',
+        progressive: true
+      }
+    })
+  ]
 });
